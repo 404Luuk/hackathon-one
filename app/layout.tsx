fe49d9cd2630b03d82e0744569e6889da15adb8a
fabiantax87 <fabiantax87@users.noreply.github.com>
2023-09-12 14:27:05 +0200

    fixed folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navigation from "./components/Navigation/Navigation";
+import Navigation from "@/components/Navigation/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
