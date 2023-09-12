023a3204a5a436c43c6d3182bf633275ea9f4f7b
fabiantax87 <74959529+fabiantax87@users.noreply.github.com>
2023-09-12 13:35:01 +0200

    Add application layout (#1)
    * added base layout
    
    * added underline on hover
    
    ---------
    
    Co-authored-by: fabiantax87 <fabiantax87@users.noreply.github.com>

---
+++ app/components/Navigation/Navigation.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+const Navigation = () => {
+  return (
+    <div className="shadow-md h-full">
+      <div className="ml-5 mt-5">
+        <h1 className="text-2xl font-bold">Welcome (name)</h1>
+        <h3>(Member type)</h3>
+      </div>
+      <ul className="ml-5 mt-14 flex flex-col gap-2">
+        <li>
+          <Link href="/personal-training" className="hover:underline">
+            Personal training
+          </Link>
+        </li>
+        <li>
+          <Link href="/courses" className="hover:underline">
+            Courses
+          </Link>
+        </li>
+        <li>
+          <Link href="/settings" className="hover:underline">
+            Settings
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
