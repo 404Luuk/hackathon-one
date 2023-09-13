da17a5b6b76fae26684434b9c63ae6f5e5c1b3e3
luuk0406 <luukf0406@gmail.com>
2023-09-13 22:24:18 +0200

    add login page

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -2,9 +2,10 @@ import { usersRepo } from "@/app/helpers/userRepo";
 
 export async function POST(request: Request) 
 {
+    console.log(request);
     const data = await request.json();
     const user = usersRepo.login(data);
-    
+
     if(!user) { throw Error; }
 
     console.log("succes");
